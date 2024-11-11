4df1ca453cab114d07745e281d67cd4470c47a66
boowerk <back20044@gmail.com>
2024-11-11 15:21:18 +0900

    새 좌우 반전 그리기 구현

--- bird.py
+++ bird.py
@@ -1,4 +1,5 @@
 # 이것은 각 상태들을 객체로 구현한 것임.
+import random
 
 from pico2d import get_time, load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT, load_font
 from state_machine import *
@@ -13,7 +14,7 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-TIME_PER_ACTION = 0.7
+TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 14
 
@@ -21,13 +22,13 @@ class Idle:
     @staticmethod
     def enter(bird, e):
         if start_event(e):
-            bird.action = 3
+            bird.action = 0
             bird.face_dir = 1
         elif right_down(e) or left_up(e):
-            bird.action = 2
+            bird.action = 1
             bird.face_dir = -1
         elif left_down(e) or right_up(e):
-            bird.action = 3
+            bird.action = 1
             bird.face_dir = 1
 
         bird.frame = 0
@@ -40,13 +41,17 @@ class Idle:
 
     @staticmethod
     def do(bird):
-        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+
+        if int(bird.frame) == 0:  # 한 줄이 끝날 때마다 실행
+            bird.action = (bird.action + 1) % 3  # 다음 줄로 넘어감, 마지막 줄 이후엔 첫 줄로 돌아감
+
         if get_time() - bird.wait_time > 2:
             bird.state_machine.add_event(('TIME_OUT', 0))
 
     @staticmethod
     def draw(bird):
-        bird.image.clip_draw(int(bird.frame) * 100, bird.action * 100, 100, 100, bird.x, bird.y)
+        bird.image.clip_draw(int(bird.frame) * 183 , (2 - bird.action) * 169, 183, 169, bird.x, bird.y)
 
 class Run:
     @staticmethod
@@ -64,19 +69,26 @@ class Run:
 
     @staticmethod
     def do(bird):
-        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+
+        if int(bird.frame) == 0:  # 한 줄이 끝날 때마다 실행
+            bird.action = (bird.action + 1) % 3  # 다음 줄로 넘어감, 마지막 줄 이후엔 첫 줄로 돌아감
 
         # bird.x += bird.dir * 5
         bird.x += bird.dir * RUN_SPEED_PPS * game_framework.frame_time
 
     @staticmethod
     def draw(bird):
-        bird.image.clip_draw(int(bird.frame) * 100, bird.action * 100, 100, 100, bird.x, bird.y)
+        if bird.face_dir == -1:  # 왼쪽을 향할 때 좌우 반전
+            bird.image.clip_composite_draw(int(bird.frame) * 183, (2 - bird.action) * 169, 183, 169, 0, 'h', bird.x,
+                                           bird.y, 183, 169)
+        else:  # 오른쪽을 향할 때는 그대로 그리기
+            bird.image.clip_draw(int(bird.frame) * 183, (2 - bird.action) * 169, 183, 169, bird.x, bird.y)
 
 class Bird:
 
     def __init__(self):
-        self.x, self.y = 400, 90
+        self.x, self.y = random.randint(100, 800), random.randint(50, 200)
         self.face_dir = 1
         self.font = load_font('ENCR10B.TTF', 16)
         self.image = load_image('bird_animation.png')
